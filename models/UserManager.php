de091d493e7e7801747720e3e8ae54ed64b40571
drizix <andriani.cesar@gmail.com>
2021-04-26 19:37:51 +0200

    somple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,12 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  $username = "\"" . $username . "\"";
+  global $PDO;
+  $response = ($PDO->query("SELECT password FROM user WHERE nickname = $username"))->fetchAll();
+  $result = ($response[0]['password'] == $password) ?  1 : -1;
+  return $result;
+}
